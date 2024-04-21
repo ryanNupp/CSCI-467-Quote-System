a524ca6c22ff2f3f637df6e45db2114d4f3d47d6
ryanNupp <ryan.nuppenau@icloud.com>
2024-04-21 00:07:35 -0500

    Frontend overhaul using Bootstrap

--- index.php
+++ index.php
@@ -4,28 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheet.css">
-    <title>Log In</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Portal</title>
 </head>
 
-<body>
-    <div class="main-content" id="center">
-        <h1>Please log in bro.</h1>
-        <form action="" method="post">
-            <li>
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username">
-            </li>
-            <li>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
-            </li>
-            <li>
-                <input name="submit" type="submit" value="submit">Log In</input>
-            </li>
+<body data-bs-theme="dark">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">site navigation (temporary)</button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="admin.html">admin</a></li>
+            <li><a class="dropdown-item" href="quote_editor.html">hq</a></li>
+            <li><a class="dropdown-item" href="associate.html">associate</a></li>
+        </ul>
+    </div>
+
+    <div class="container position-absolute top-50 start-50 translate-middle" style="width: 300px;">
+        <h1>Please log in</h1>
+
+        <!-- Login Form -->
+        <form method="post" class="align-items-center">
+            <!-- Username Input -->
+            <div class="col-auto mb-3">
+                <label for="username" class="visually-hidden">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+            </div>
+
+            <!-- Password Input -->
+            <div class="col-auto mb-3">
+                <label for="password" class="visually-hidden">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+
+            <!-- Submit Button -->
+            <div class="col-auto mb-3">
+                <input class="col-auto btn btn-primary" type="submit" name="submit" value="Log In">
+            </div>
         </form>
     </div>
 
+    <!-- Login functionality with PHP, compares user input username & password against users in database -->
     <?php
         session_start();
 
@@ -76,21 +94,6 @@
         }
     ?>
 
-    <div class="main-content" id="center">
-        <p>Temporary test buttons while log in functionality is being worked out:</p>
-        <a href="admin.html">
-            <button>Admin Interface</button>
-        </a>
-        <a href="associate.html">
-            <button>Associate Interface</button>
-        </a>
-        <a href="quote_editor.html">
-            <button>HQ Interface</button>
-        </a>
-        <a href="confirmation.html">
-            <button>Customer confirmation</button>
-        </a>
-    </div>
 </body>
 
 </html>
